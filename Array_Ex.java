7d48b51c5fbc20820dba1eb85e63900dfc47dd10
YUGAN-SVR <153530074+YUGAN-SVR@users.noreply.github.com>
2025-03-19 15:29:17 +0530

    Session 3

---
+++ Array_Ex.java
@@ -0,0 +1,16 @@
+package session3;
+
+public class Array_Ex {
+
+	public static void main(String[] args) {
+		String[] names= {"virat" , "sachin" , "Dhoni"};
+		
+		//Acces all Elements using for loop
+		/*for(int i=0; i<names.length;i++) {
+			System.out.println(names[i]);
+		}*/
+		//System.out.println(names[2]);
+	
+		                                        
+	}
+}
---
+++ LooingStatements.java
@@ -0,0 +1,32 @@
+package session3;
+
+public class LooingStatements {
+	
+	public static void main(String[] args) {
+		
+	//int i=1;
+	
+			/*while(i<10) {
+				
+				System.out.println("value of i: "+i);
+				i++;
+			}
+			System.out.println("Final value of i is: "+i);*/	
+			
+			/*int i=10;
+			do {
+				System.out.println("value of i: "+i);
+				i--;
+			}while(i>=1);
+			System.out.println("Final value of i is: "+i);*/
+		
+		int i;
+		for(i=1;i<=15;i++) {
+			System.out.println("The value of i is: "+i);
+			
+		}
+		System.out.println("The final value of i is: "+i);
+		
+	}		
+
+}
